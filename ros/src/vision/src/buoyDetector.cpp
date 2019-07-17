9e81ffbd2c1262e8de24e796a8f4bbc09c04c243
Robert Keen <robertkeen9@gmail.com>
2019-07-17 14:53:11 -0700

    reduced line length and added a demo method

--- ros/src/vision/src/buoyDetector.cpp
+++ ros/src/vision/src/buoyDetector.cpp
@@ -25,7 +25,9 @@ private:
         cv::Ptr<cv::xfeatures2d::SIFT> sift = cv::xfeatures2d::SIFT::create();
         std::vector<cv::KeyPoint> keypoints1, keypoints2;
         cv::Mat descriptors1, descriptors2;
-        cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED);
+        cv::Ptr<cv::DescriptorMatcher> matcher = 
+            cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED);
+
     };
     Detector detector;
 public:
@@ -34,19 +36,26 @@ public:
         this->cap = cap;
         switch(buoy_type) {
             case Jiangshi:
-                this->detector.buoy_img = cv::imread("../images/Jiangshi_lg.png", cv::IMREAD_GRAYSCALE);
+                this->detector.buoy_img 
+                    = cv::imread("../images/Jiangshi_lg.png", cv::IMREAD_GRAYSCALE);
                 break;
             case Aswang:
-                this->detector.buoy_img = cv::imread("../images/Aswang_lg.png", cv::IMREAD_GRAYSCALE);
+                this->detector.buoy_img 
+                    = cv::imread("../images/Aswang_lg.png", cv::IMREAD_GRAYSCALE);
                 break;
             case Draugr:
-                this->detector.buoy_img = cv::imread("../images/Draugr_lg.png", cv::IMREAD_GRAYSCALE);
+                this->detector.buoy_img 
+                    = cv::imread("../images/Draugr_lg.png", cv::IMREAD_GRAYSCALE);
                 break;
             case Vetalas:
-                this->detector.buoy_img = cv::imread("../images/Vetalas_lg.png", cv::IMREAD_GRAYSCALE);
+                this->detector.buoy_img 
+                    = cv::imread("../images/Vetalas_lg.png", cv::IMREAD_GRAYSCALE);
                 break;
         }
-       this->detector.sift->detectAndCompute(this->detector.buoy_img, cv::noArray(), this->detector.keypoints1, this->detector.descriptors1); 
+       this->detector.sift->detectAndCompute(this->detector.buoy_img, 
+                                            cv::noArray(),
+                                            this->detector.keypoints1, 
+                                            this->detector.descriptors1); 
 
         
     }
@@ -60,15 +69,21 @@ public:
         // Convert the frame to black and white
         cv::cvtColor(frame, gray_frame, cv::COLOR_BGR2GRAY);
 
-        detector.sift->detectAndCompute(gray_frame, cv::noArray(), detector.keypoints2, detector.descriptors2);
+        detector.sift->detectAndCompute(gray_frame, 
+                                        cv::noArray(), 
+                                        detector.keypoints2, 
+                                        detector.descriptors2);
         // matching the descriptor vectors with a FLANN based matcher
         std::vector< std::vector<cv::DMatch> > knn_matches;
-        detector.matcher->knnMatch( detector.descriptors1, detector.descriptors2, knn_matches, 2);
+        detector.matcher->knnMatch( detector.descriptors1, 
+                                    detector.descriptors2, 
+                                    knn_matches, 
+                                    2);
 
         // Filter matches using the Lowe's ratio test
         std::vector<cv::DMatch> good_matches;
         for (size_t i = 0; i < knn_matches.size(); i++){
-            if ( knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance){
+            if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance){
                 good_matches.push_back(knn_matches[i][0]);
             }
         }
@@ -84,8 +99,10 @@ public:
             cv::Mat H = cv::findHomography(src_pts, dst_pts, cv::RANSAC);
             // get the corners from the jiangshi image
             std::vector<cv::Point2f> obj_corners(4);
-            obj_corners[0] = cvPoint(0,0); obj_corners[1] = cvPoint( detector.buoy_img.cols, 0 );
-            obj_corners[2] = cvPoint( detector.buoy_img.cols, detector.buoy_img.rows ); obj_corners[3] = cvPoint( 0, detector.buoy_img.rows );
+            obj_corners[0] = cvPoint(0,0); obj_corners[1] = 
+                cvPoint( detector.buoy_img.cols, 0 );
+            obj_corners[2] = cvPoint(detector.buoy_img.cols, detector.buoy_img.rows); 
+            obj_corners[3] = cvPoint(0, detector.buoy_img.rows);
             std::vector<cv::Point2f> scene_corners(4);
             
             if ( !H.empty() ){
@@ -97,9 +114,18 @@ public:
         }
         return found_buoy;
     } 
+
     cv::Rect GetRect() // returns a cv::Rect if the buoy is found
     {
         if(FindBuoy())
             return buoy_rect;
+    }
+    
+    void Demo()
+    {
+        if(FindBuoy()){
+            cv::rectangle(frame, buoy_rect, cv::Scalar(0, 0, 255), 3);
+        }
+        cv::imshow("frame", frame); 
     }
 }; 
