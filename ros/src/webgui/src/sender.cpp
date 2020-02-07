ba19f0047b00efbc3bcd50ea0d35cb87c6aa1457
Oscar <oscarmorrison@Omorr-16.04-T420>
2020-02-06 17:32:45 -0800

    Syncing to development while moving reciever.cpp to webgui

---
+++ ros/src/webgui/src/sender.cpp
@@ -0,0 +1,61 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/videoio.hpp>
+//#include <opencv2/videoio.hpp>
+
+int main (int argc, char ** argv)
+{
+    ros::init(argc, argv, "image_publisher");
+    ros::NodeHandle nh("~");
+    image_transport::ImageTransport it(nh);
+
+    std::string topic_name, fd;
+    int fps;
+    bool is_device;
+    nh.getParam("topic_name", topic_name);
+    nh.getParam("fd", fd);
+    nh.getParam("fps", fps);
+    nh.getParam("is_device", is_device);
+
+    std::string publisher_name = "/video/" + topic_name;
+    image_transport::Publisher publisher = it.advertise(publisher_name, 5);
+
+    cv::VideoCapture source;
+    if (is_device) {
+        uint8_t video_index = (uint8_t) (fd.back() - '0');
+        source = cv::VideoCapture(video_index);
+    } else {
+        source = cv::VideoCapture(fd);
+    }
+
+    if (!source.isOpened()) {
+        ROS_ERROR("Failed to open device on %s", fd.c_str());
+        return -1;
+    }
+
+    ROS_INFO("Opened camera on %s", fd.c_str());
+    ros::Rate loop_rate(fps);
+
+    while(nh.ok())
+    {
+        cv::Mat frame;
+        source >> frame;
+
+        if (frame.empty()) {
+            continue;
+        }
+
+        // Convert to ROS image and send out
+        sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+        publisher.publish(msg);
+
+        // Spin and wait
+        cv::waitKey(1);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
--- ros/test_cameras.md
+++ ros/test_cameras.md
@@ -1,11 +1,11 @@
 # Architecture Decision Record - Webgui and Camera Tests
-## Proposal
+## Status: Proposal
 
 ## Context
-the development webgui has only minor script changes from the 2018 Polaris source code and launches moved into the 2018 package.
-There are minor layout and load order problems in the webgui and  camera_test.launch requres sender and reciever exicutables to fuction in the development branch.
+The development webgui has only minor script changes from the 2018 Polaris source code and launches when it is moved into the 2018 package.
+There are minor layout and load order problems in the webgui. The camera_test.launch requres sender and reciever exicutables to fuction in the development branch.
 ## Decision
 change the load order of bootsrap scripts in the webgui. Bring over sender and reciever files from the 2018 code base and configure the their exicutables in the vision node.
 ## Consequences
-As a result of the changes the webgui now conforms to convention on bootstrap load order and the camera_test.launch file will start in the development branch.
+As a result of the changes the webgui now conforms to convention on bootstrap load order and the camera_test.launch file starts in the development branch.
 However the sender and reciever additions may only be used by camera testing files. 
