82b3431eb2cfbd5379a139b3a8802b219e5c4152
Oscar <oscarmorrison@Omorr-16.04-T420>
2020-02-14 14:38:19 -0800

    webgui now supports raw video streaming independent of vision, sender and reciever moved to webgui/src and webgui package dependencies modified.

--- ros/src/webgui/src/reciever.cpp
+++ ros/src/webgui/src/reciever.cpp
@@ -2,7 +2,7 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
-#include <opencv2/videoio.hpp>
+//#include <opencv2/videoio.hpp>
 
 std::string name;
 
